e8f6ada0c2d0336deb4762a41d5051dd375cbe69
RDP77 <ravidwiputra@gmail.com>
2020-06-11 10:39:14 +0700

    Update Certification

--- js/glitche-scripts.js
+++ js/glitche-scripts.js
@@ -33,7 +33,7 @@ $(function () {
 	$(window).load(function () {
 		$("body").css("overflow", "hidden");
 		$(".cursor").css("display", "none");
-		$(".preloader .pre-inner").fadeOut(5000, function () {
+		$(".preloader .pre-inner").fadeOut(3000, function () {
 			/* Preload hide */
 			$('.preloader').fadeOut();
 			$('body').addClass('loaded');
