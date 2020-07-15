bebe7cb250754293104e7d9f699a9d64cea34e33
RDP77 <ravidwiputra@gmail.com>
2020-07-16 02:53:25 +0700

    TESTO

--- assets/js/glitche-scripts.js
+++ assets/js/glitche-scripts.js
@@ -4,12 +4,9 @@ $(function () {
   "use strict";
 
   /* Check Link */
-  // if (
-  //   window.location.href !== "www.ravidwiputra.my.id" ||
-  //   window.location.href !== "ravidwiputra.my.id"
-  // ) {
-  //   window.location.replace("https://ravidwiputra.my.id");
-  // }
+  if (window.location.hostname == "rdp77.github.io") {
+    window.location.replace("https://ravidwiputra.my.id");
+  }
   console.log(window.location.href);
   console.log(window.location.hostname);
 
