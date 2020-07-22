de9925ab676a55f23556f9e60b83697bda55d589
RDP77 <ravidwiputra@gmail.com>
2020-07-22 15:01:36 +0700

    Update

--- assets/js/glitche-scripts.js
+++ assets/js/glitche-scripts.js
@@ -82,23 +82,6 @@ $(function () {
     return false;
   });
 
-  /*Pop Up*/
-  function popup() {
-    var qs, js, q, s, d = document,
-      gi = d.getElementById,
-      ce = d.createElement,
-      gt = d.getElementsByTagName,
-      id = "typef_orm_share",
-      b = "https://embed.typeform.com/";
-    if (!gi.call(d, id)) {
-      js = ce.call(d, "script");
-      js.id = id;
-      js.src = b + "embed.js";
-      q = gt.call(d, "script")[0];
-      q.parentNode.insertBefore(js, q)
-    }
-  };
-
   /*Menu mobile*/
   $("header").on("click", ".menu-btn", function () {
     if ($("header").hasClass("active")) {
---
+++ contact.md
@@ -0,0 +1,6 @@
+---
+title: Contact | Moh Ravi Dwi Putra
+titlecontent: Contact
+layout: contact
+permalink: /contact
+---
