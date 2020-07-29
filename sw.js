0948e88f1fbbbb0cbc80c6216886a443d1918abd
RDP77 <ravidwiputra@gmail.com>
2020-07-29 21:02:05 +0700

    The Updates

--- sw.js
+++ sw.js
@@ -1,8 +1,25 @@
 importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js"
+  "https://unpkg.com/workbox-sw@0.0.2/build/importScripts/workbox-sw.dev.v0.0.2.js"
 );
+importScripts(
+  "https://unpkg.com/workbox-runtime-caching@1.3.0/build/importScripts/workbox-runtime-caching.prod.v1.3.0.js"
+);
+importScripts(
+  "https://unpkg.com/workbox-routing@1.3.0/build/importScripts/workbox-routing.prod.v1.3.0.js"
+);
+
+const assetRoute = new workbox.routing.RegExpRoute({
+  regExp: new RegExp("^/assets/img/*"),
+  handler: new workbox.runtimeCaching.CacheFirst(),
+});
+
+const router = new workbox.routing.Router();
+//router.addFetchListener();
+router.registerRoutes({ routes: [assetRoute] });
+router.setDefaultHandler({
+  handler: new workbox.runtimeCaching.CacheFirst(),
+});
 var CACHE_NAME = "rdp77-cache-v1";
-const IMAGE_CACHE = "/assets/img";
 var urlsToCache = [
   "/",
   "/index.html",
@@ -11,18 +28,6 @@ var urlsToCache = [
   "/awards",
   "/offline.html",
 ];
-
-workbox.routing.registerRoute(
-  ({ event }) => event.request.destination === "/assets/img",
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: IMAGE_CACHE,
-    plugins: [
-      new workbox.expiration.ExpirationPlugin({
-        maxEntries: 15,
-      }),
-    ],
-  })
-);
 
 self.addEventListener("install", function (event) {
   // Perform install steps
