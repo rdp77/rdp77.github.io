84be660f83e94842ce8dfa4cce596b5554578b21
RDP77 <ravidwiputra@gmail.com>
2020-08-11 11:05:01 +0700

    Clean File

--- sw.js
+++ sw.js
@@ -1,3 +1,25 @@
+importScripts(
+  "https://unpkg.com/workbox-sw@0.0.2/build/importScripts/workbox-sw.dev.v0.0.2.js"
+);
+importScripts(
+  "https://unpkg.com/workbox-runtime-caching@1.3.0/build/importScripts/workbox-runtime-caching.prod.v1.3.0.js"
+);
+importScripts(
+  "https://unpkg.com/workbox-routing@1.3.0/build/importScripts/workbox-routing.prod.v1.3.0.js"
+);
+
+const assetRoute = new workbox.routing.RegExpRoute({
+  regExp: new RegExp("^/assets/img/*"),
+  handler: new workbox.runtimeCaching.CacheFirst(),
+});
+
+const router = new workbox.routing.Router();
+//router.addFetchListener();
+router.registerRoutes({ routes: [assetRoute] });
+router.setDefaultHandler({
+  handler: new workbox.runtimeCaching.CacheFirst(),
+});
+
 var CACHE_NAME = "rdp77-cache-v1";
 var urlsToCache = [
   "/",
