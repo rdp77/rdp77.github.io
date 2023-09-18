14c48dc0605b00a7f806668d772c4718f6c5e92a
Moh Ravi Dwi Putra <ravidwiputra@gmail.com>
2023-09-18 11:19:54 +0700

    Add default lang HTML

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 const nextConfig = {
+    i18n: {
+        locales: ["en", "id"],
+        defaultLocale: "en",
+    },
     env: {
         APP_URL: process.env.APP_URL,
         APP_EMAIL: process.env.APP_EMAIL,
