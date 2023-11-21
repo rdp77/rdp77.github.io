51da6e321caf1087030c52230d20ee906c06c8e3
Moh Ravi Dwi Putra <ravidwiputra@gmail.com>
2023-11-21 20:38:11 +0700

    add ahrefs verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,7 @@ export const metadata: Metadata = {
         google: '8FB68inDTkKFOhWeQv2l6zfpF7oYJEn6wqZDOTDd6dk',
     },
     openGraph: {
-        type:'profile',
+        type: 'profile',
         firstName: 'Moh Ravi',
         lastName: 'Dwi Putra',
         gender: 'Male',
@@ -75,80 +75,79 @@ export const metadata: Metadata = {
 
 const RootLayout: React.FC<LayoutProps> = ({children}) => {
     return (
-        <>
-            <html className={robotoMono.className} lang="en">
-            <head>
-                {/* eslint-disable-next-line @next/next/no-page-custom-font */}
-                <link
-                    href="https://fonts.googleapis.com/css?family=Roboto+Mono:400,100,300italic,300,100italic,400italic,500,500italic,700,700italic&amp;subset=latin,cyrillic&display=swap"
-                    rel="stylesheet"
-                />
-                {/* eslint-disable-next-line @next/next/no-css-tags */}
-                <link rel="stylesheet" href="/assets/css/basic.css"/>
-                {/* eslint-disable-next-line @next/next/no-css-tags */}
-                <link rel="stylesheet" href="/assets/css/layout.css"/>
-                {/* eslint-disable-next-line @next/next/no-css-tags */}
-                <link rel="stylesheet" href="/assets/css/animate.min.css"/>
-                {/* eslint-disable-next-line @next/next/no-css-tags */}
-                <link rel="stylesheet" href="/assets/css/orange.css"/>
-                {/* eslint-disable-next-line @next/next/no-css-tags */}
-                <link rel="stylesheet" href="/assets/css/dark.css"/>
+        <html className={robotoMono.className} lang="en">
+        <head>
+            {/* eslint-disable-next-line @next/next/no-page-custom-font */}
+            <link
+                href="https://fonts.googleapis.com/css?family=Roboto+Mono:400,100,300italic,300,100italic,400italic,500,500italic,700,700italic&amp;subset=latin,cyrillic&display=swap"
+                rel="stylesheet"
+            />
+            {/* eslint-disable-next-line @next/next/no-css-tags */}
+            <link rel="stylesheet" href="/assets/css/basic.css"/>
+            {/* eslint-disable-next-line @next/next/no-css-tags */}
+            <link rel="stylesheet" href="/assets/css/layout.css"/>
+            {/* eslint-disable-next-line @next/next/no-css-tags */}
+            <link rel="stylesheet" href="/assets/css/animate.min.css"/>
+            {/* eslint-disable-next-line @next/next/no-css-tags */}
+            <link rel="stylesheet" href="/assets/css/orange.css"/>
+            {/* eslint-disable-next-line @next/next/no-css-tags */}
+            <link rel="stylesheet" href="/assets/css/dark.css"/>
 
-                {/*Meta Robots Search*/}
-                <meta charSet="UTF-8"/>
-                <meta httpEquiv="Content-Language" content="all-language"/>
-                <meta httpEquiv="X-UA-Compatible" content="IE=Edge"/>
-                <meta name="geo.placename" content="Indonesia"/>
-                <meta name="geo.country" content="id"/>
-                <meta name="geo.region" content="ID-BT"/>
-                <meta name="language" content="id"/>
-                <meta name="target" content="global"/>
-                <meta name="distribution" content="global"/>
-                <meta name="rating" content="general"/>
-                <meta name="revisit-after" content="1 days"/>
-                <meta name="MSSmartTagsPreventParsing" content="true"/>
-                <meta name="Googlebot-Image" content="follow, all"/>
-                <meta name="msnbot" content="follow, all"/>
-                <meta name="Slurp" content="follow, all"/>
-                <meta name="ZyBorg" content="follow, all"/>
-                <meta name="Scooter" content="follow, all"/>
-                <meta name="spiders" content="all"/>
-                <meta name="WEBCRAWLERS" content="all"/>
-                <meta
-                    name="search engines"
-                    content="aeiwi, alexa, alltheWeb, altavista, aol netfind, anzwers, canada, directhit, euroseek, excite, overture, go, google, hotbot. infomak, kanoodle, lycos, mastersite, national directory, northern light, searchit, simplesearch, Websmostlinked, webtop, what-u-seek, aol, yahoo, webcrawler, infoseek, excite, magellan, looksmart, bing, cnet, googlebot"
-                />
+            {/*Meta Robots Search*/}
+            <meta charSet="UTF-8"/>
+            <meta httpEquiv="Content-Language" content="all-language"/>
+            <meta httpEquiv="X-UA-Compatible" content="IE=Edge"/>
+            <meta name="geo.placename" content="Indonesia"/>
+            <meta name="geo.country" content="id"/>
+            <meta name="geo.region" content="ID-BT"/>
+            <meta name="language" content="id"/>
+            <meta name="target" content="global"/>
+            <meta name="distribution" content="global"/>
+            <meta name="rating" content="general"/>
+            <meta name="revisit-after" content="1 days"/>
+            <meta name="MSSmartTagsPreventParsing" content="true"/>
+            <meta name="Googlebot-Image" content="follow, all"/>
+            <meta name="msnbot" content="follow, all"/>
+            <meta name="Slurp" content="follow, all"/>
+            <meta name="ZyBorg" content="follow, all"/>
+            <meta name="Scooter" content="follow, all"/>
+            <meta name="spiders" content="all"/>
+            <meta name="WEBCRAWLERS" content="all"/>
+            <meta
+                name="search engines"
+                content="aeiwi, alexa, alltheWeb, altavista, aol netfind, anzwers, canada, directhit, euroseek, excite, overture, go, google, hotbot. infomak, kanoodle, lycos, mastersite, national directory, northern light, searchit, simplesearch, Websmostlinked, webtop, what-u-seek, aol, yahoo, webcrawler, infoseek, excite, magellan, looksmart, bing, cnet, googlebot"
+            />
+            <meta name="ahrefs-site-verification" content="b850a8261da5283732a7d275f2ca5d60d7241d12a5e3ed0e6f344d91cb22721c"/>
 
-                {/*Color*/}
-                <meta content="#26272c" name="theme-color"/>
-                <meta content="#26272c" name="msapplication-navbutton-color"/>
-                <meta content="#26272c" name="apple-mobile-web-app-status-bar-style"/>
+            {/*Color*/}
+            <meta content="#26272c" name="theme-color"/>
+            <meta content="#26272c" name="msapplication-navbutton-color"/>
+            <meta content="#26272c" name="apple-mobile-web-app-status-bar-style"/>
 
-                {/*Favicon*/}
-                <link href={iconAssets.favicon} rel="shortcut icon" type='image/x-icon'/>
-                <link rel="apple-touch-icon" sizes="180x180" href={iconAssets.icon180}/>
-                <link rel="icon" type="image/png" sizes="192x192" href={iconAssets.icon192}/>
-                <link rel="icon" type="image/png" sizes="32x32" href={iconAssets.icon32}/>
-                <link rel="icon" type="image/png" sizes="96x96" href={iconAssets.icon96}/>
-                <link rel="icon" type="image/png" sizes="16x16" href={iconAssets.icon16}/>
-                <meta name="msapplication-TileImage" content={iconAssets.icon144}/>
-            </head>
-            <body>
-            <main>
-                {children}
-            </main>
-            </body>
-            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-51LP1W0ZK4"/>
-            <Script id="google-analytics">
-                {`
+            {/*Favicon*/}
+            <link href={iconAssets.favicon} rel="shortcut icon" type='image/x-icon'/>
+            <link rel="apple-touch-icon" sizes="180x180" href={iconAssets.icon180}/>
+            <link rel="icon" type="image/png" sizes="192x192" href={iconAssets.icon192}/>
+            <link rel="icon" type="image/png" sizes="32x32" href={iconAssets.icon32}/>
+            <link rel="icon" type="image/png" sizes="96x96" href={iconAssets.icon96}/>
+            <link rel="icon" type="image/png" sizes="16x16" href={iconAssets.icon16}/>
+            <meta name="msapplication-TileImage" content={iconAssets.icon144}/>
+        </head>
+        <body>
+        <main>
+            {children}
+        </main>
+        </body>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-51LP1W0ZK4"/>
+        <Script id="google-analytics">
+            {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
                     gtag('config', 'G-51LP1W0ZK4');
                 `}
-            </Script>
-            </html>
-        </>
+        </Script>
+        </html>
     );
 };
 
